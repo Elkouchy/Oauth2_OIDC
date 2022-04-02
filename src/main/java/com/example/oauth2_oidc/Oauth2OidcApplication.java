b68543251736b22b625a5c57c83f926d88e6125e
DevSquad <devsquad2021@gmail.com>
2022-04-02 15:19:09 +0200

    first commit

--- src/main/java/com/example/oauth2_oidc/Oauth2OidcApplication.java
+++ src/main/java/com/example/oauth2_oidc/Oauth2OidcApplication.java
@@ -2,10 +2,17 @@ package com.example.oauth2_oidc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Oauth2OidcApplication {
-
+	
+	@GetMapping("/messages")
+	public String messages() {
+		return "hello world";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2OidcApplication.class, args);
 	}
